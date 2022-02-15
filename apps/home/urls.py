344ada9ad8ab8b894913dab9206440ee3ee27f22
Fatemask <fatemask29@gmail.com>
2022-02-15 10:41:15 +0530

    c

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -9,7 +9,10 @@ from apps.home import views
 urlpatterns = [
 
     # The home page
-    path('', views.index, name='home'),
+    path(' ', views.index, name='home'),
+
+    # The 
+    # path('', views.index, name='home'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
